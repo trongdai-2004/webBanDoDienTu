0c273bb81a073c840db60a5224692b5e574dff23
Trong Dai <caotrongdai2004cd@gmail.com>
2026-04-21 16:03:43 +0700

    21 tối ưu phần tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,6 +2,7 @@
 const Product =require("../../models/product.model")
 
 const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
 
 
 // [GET] /admin/products
@@ -10,36 +11,38 @@ module.exports.index = async (req, res) => {
 
     const filterStatus = filterStatusHelper(req.query);
 
+  // end bộ lọc
 
-  // end Đoạn bộ lọc
+
 
 
     let find = {
       deleted: false,
-      
 
     }
     if(req.query.status){
       find.status = req.query.status;
     }
 
-    let keyword = "";
-    if(req.query.keyword){
-      keyword = req.query.keyword;
+    const objectSearch = searchHelper(req.query);
 
-      const regex = new RegExp(keyword, "i")
-      find.title = regex;
+    console.log(objectSearch)
+
+    
+    if(objectSearch.regex){
+    
+      find.title = objectSearch.regex;
     }
 
     
 
     const products =  await Product.find(find)
 
-    // console.log(products)
+
     res.render("admin/pages/products/index",{
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
-    keyword: keyword
+    keyword: objectSearch.keyword
   });
 };
